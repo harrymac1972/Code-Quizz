071ddcde5bb733cedf2c6c2f4402f099a9308b2c
harry <harrymac1972@gmail.com>
2023-10-30 04:20:44 -0600

    mockup timer working to from 3 to 0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,28 @@
 
 var highScoresEl = document.querySelector("#high-scores-link");
 var timeValue = document.querySelector("#time-value");
+var startQuizBtn = document.querySelector("#start-quiz-btn");
+var timerSeconds = 0;
+var timerInterval = 0;
 
-timeValue.innerText=0;
+startQuizBtn.addEventListener("click",start);
+
+function start(){
+    timerSeconds = 3;
+    timeValue.innerText = timerSeconds;
+    timerInterval = setInterval(myTimer, 1000);
+    return
+}
+
+function myTimer() {
+    timerSeconds--;
+    timeValue.innerText = timerSeconds;
+    if (timerSeconds <= 0) {
+        clearInterval(timerInterval);
+    }
+  }
+
+timeValue.innerText = timerSeconds;
+
+
 
